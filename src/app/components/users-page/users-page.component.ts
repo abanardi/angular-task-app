587bad9af5bbccd8c31f51753bad82a508cdf90f
Andrew Banardi <abanardi0@gmail.com>
2025-05-28 01:17:55 -0400

    Kind of fixing colros

--- src/app/components/users-page/users-page.component.ts
+++ src/app/components/users-page/users-page.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { UserTableComponent } from '../user-table/user-table.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-users-page',
   standalone: true,
-  imports: [UserTableComponent, MatInputModule, MatFormFieldModule],
+  imports: [UserTableComponent, MatInputModule, MatFormFieldModule, MatCardModule],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.scss',
 })
