46261796fc536b3343194346b1ea0140f58a5d6f
Andrew Banardi <andrewbanardi@Andrews-MacBook-Pro.local>
2025-06-05 08:44:14 -0400

    Added the tasks cards properly

--- src/app/components/tasks-page/tasks-page.component.ts
+++ src/app/components/tasks-page/tasks-page.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { TaskListComponent } from '../task-list/task-list.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tasks-page',
   standalone: true,
-  imports: [MatButtonModule, TaskListComponent],
+  imports: [
+    MatButtonModule,
+    TaskListComponent,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './tasks-page.component.html',
   styleUrl: './tasks-page.component.scss',
 })
