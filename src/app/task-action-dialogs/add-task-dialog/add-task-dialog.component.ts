429f04a55d67f3e9b84338056f95cb196948a6b5
Andrew Banardi <abanardi0@gmail.com>
2025-06-10 23:44:54 -0400

    Added some buttons in add task dialog

--- src/app/task-action-dialogs/add-task-dialog/add-task-dialog.component.ts
+++ src/app/task-action-dialogs/add-task-dialog/add-task-dialog.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-task-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './add-task-dialog.component.html',
-  styleUrl: './add-task-dialog.component.scss'
+  styleUrl: './add-task-dialog.component.scss',
 })
 export class AddTaskDialogComponent {
+  constructor(private dialogRef: MatDialogRef<AddTaskDialogComponent>) {}
 
+  onClose() {
+    this.dialogRef.close();
+  }
+
+  addTask() {
+    console.log('Added a task');
+    this.dialogRef.close();
+  }
 }
